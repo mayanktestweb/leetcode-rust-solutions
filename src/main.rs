e449c688e26ec991eace0e285f2e7354f1a56727
Mayank <cmayank949@gmail.com>
2022-06-24 11:53:32 +0530

    Leetcode 9: Palindrome Number

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,37 @@
-use std::collections::HashMap;
-
 struct Solution {
 
 }
 
 impl Solution {
-    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut map: HashMap<&i32, Vec<i32>> = HashMap::new();
-
-        for (index, num) in nums.iter().enumerate() {
-            let indicis = map.entry(num).or_insert(vec![]);
-            indicis.push(index as i32);
+    pub fn is_palindrome(x: i32) -> bool {
+        if x < 0 {
+            return false;
         }
 
-        let mut ans = vec![];
+        let mut digits = vec![];
 
-        for (i, num) in nums.iter().enumerate() {
-            let num_to_find: i32 = target - num;
-            let indicis = match map.get(&num_to_find) {
-                Some(arr) => arr.to_owned(),
-                None => vec![]
-            };
-            if indicis.len() == 0 {
-                continue;
-            }
+        let mut num = x;
 
-            else {
-                for index in indicis {
-                    if index as i32 != i as i32 {
-                        ans.push(index as i32);
-                        ans.push(i as i32);
-                        break;
-                    } else {
-                        continue;
-                    }
-                }
-            }
-            if ans.len() > 0 {
-                break;
-            }
+        while num > 0 {
+            digits.push(num%10);
+            num = num/10;
+        }
+        let mut val = 0;
+        for i in (0..digits.len()).rev() {
+            let mut num: i32 = 10;
+            num = num.pow(digits.len() as u32 - 1 - i as u32);
+            val += digits[i]*num
         }
 
-        return ans;
+        return val == x;
+
+        unreachable!()
     }
 }
 
 fn main() {
-    let ans = Solution::two_sum(vec![2,7,11,15,2], 9);
-    println!("{:?}", ans);
+    let ans = Solution::is_palindrome(121);
+    println!("{ans}");
 }
 
 
